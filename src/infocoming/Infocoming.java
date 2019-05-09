589807d6a91e3d372904c7a55060af15edf612f8
Guirdo <produccionesaldo@gmail.com>
2019-05-09 10:28:50 -0500

    Proyecto creado

---
+++ src/infocoming/Infocoming.java
@@ -0,0 +1,19 @@
+/*
+ * Hola!
+ */
+package infocoming;
+
+/**
+ *
+ * 
+ */
+public class Infocoming {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
