2fd48e5ed9fe9d822cb0b14f27aba83c250dc430
dianavzqx <diannavazma@gmail.com>
2019-05-10 13:24:04 -0500

    pantalla vBajaAlumno

---
+++ src/mx/edu/itch/isc/infocoming/interfacesgraficas/VBajaAlumno.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.itch.isc.infocoming.interfacesgraficas;
+
+import java.awt.Font;
+import java.awt.TextField;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import net.miginfocom.swing.MigLayout;
+
+/**
+ *
+ * @author diann
+ */
+public class VBajaAlumno extends JFrame{
+    
+    private JLabel titulo1, titulo2, matricula, nombre, curso;
+    private TextField buscar;
+    private JButton btn;
+    private JTable tabla;
+    
+    public VBajaAlumno(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Baja Alumno.");
+        this.setLayout(new MigLayout("debug, wrap 3", "[][][]","[]15[][200]20[]5[]5[]5[][]20" ));
+        
+        titulo1 = new JLabel ("Alumnos inscritos");
+        titulo1.setFont(new Font("Arial",2,16));
+        titulo2 = new JLabel ("Datos del alumno");
+        titulo2.setFont(new Font("Arial",2,14));
+        matricula = new JLabel ("Matrícula: ");
+        matricula.setFont(new Font("Arial",2,12));
+        nombre = new JLabel ("Nombre: ");
+        nombre.setFont(new Font("Arial",2,12));
+        curso = new JLabel ("Curso: ");
+        curso.setFont(new Font("Arial",2,12));
+        buscar = new TextField("Nombre alumno..");
+        buscar.setFont(new Font("Arial",2,12));
+        tabla = new JTable();
+        btn = new JButton("Dar de baja");
+
+        
+        this.add(titulo1,"span 3 1");
+        this.add(buscar, "span 3 1, right");
+        this.add(new JScrollPane(tabla),"span 3 1");
+        this.add(titulo2, "span 3 1");
+        this.add(matricula,"wrap");
+        this.add(nombre,"wrap");
+        this.add(curso, "wrap");
+        this.add(btn, "span 3 1, right");
+        this.pack();
+        this.setLocationRelativeTo(null);
+    }
+}
--- src/mx/edu/itch/isc/infocoming/principal/Principal.java
+++ src/mx/edu/itch/isc/infocoming/principal/Principal.java
@@ -1,9 +1,13 @@
 package mx.edu.itch.isc.infocoming.principal;
 
+import mx.edu.itch.isc.infocoming.interfacesgraficas.VBajaAlumno;
+
 public class Principal {
     
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        VBajaAlumno vBajaA = new VBajaAlumno();
+        vBajaA.setVisible(true);
     }
 
 }
