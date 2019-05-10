5796f23b2382b4c4e8a027a05dcc23d081619ac4
tonii21 <antonio.pumas.aa@gmail.com>
2019-05-10 13:25:21 -0500

    pantallavgestionpersonal

--- src/mx/edu/itch/isc/infocoming/principal/Principal.java
+++ src/mx/edu/itch/isc/infocoming/principal/Principal.java
@@ -1,10 +1,14 @@
 package mx.edu.itch.isc.infocoming.principal;
 
+import mx.edu.itch.isc.infocoming.interfacesgraficas.VGestionPersonal;
+
 public class Principal {
     
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Hola amikos");
+        VGestionPersonal ejemplo=new VGestionPersonal();
+        ejemplo.setVisible(true);
     }
 
 }
