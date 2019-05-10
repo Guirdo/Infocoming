a9ba64614122dbbd45ee3684e1cb70da10ce8261
Guirdo <produccionesaldo@gmail.com>
2019-05-10 18:30:40 -0500

    Ahora si es el ultimo

--- src/mx/edu/itch/isc/infocoming/interfacesgraficas/VGestionPersonal.java
+++ src/mx/edu/itch/isc/infocoming/interfacesgraficas/VGestionPersonal.java
@@ -10,7 +10,6 @@ import java.awt.HeadlessException;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import net.miginfocom.swing.MigLayout;
