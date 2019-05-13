8d6dc27e30e93c3f7b9a44073b69ad6712945535
Guirdo <produccionesaldo@gmail.com>
2019-05-13 18:14:53 -0500

    Conexion a la base de datos

---
+++ src/mx/edu/itch/isc/infocoming/interfacesbd/InterfazBDUsuario.java
@@ -0,0 +1,19 @@
+package mx.edu.itch.isc.infocoming.interfacesbd;
+
+public class InterfazBDUsuario {
+    
+    private InterfazBD interfaz;
+
+    public InterfazBDUsuario(InterfazBD interfaz) {
+        this.interfaz = interfaz;
+    }
+    
+    //TODO Aqui van los metodos
+    private void modificarContrasena(){
+        
+    }
+    
+    private void consultarAlumnos(){
+        
+    }
+}
