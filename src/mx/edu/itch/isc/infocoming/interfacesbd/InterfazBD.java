9052bd4aa62cfbf802ae4b226cc4a86f0baf2a0b
Guirdo <produccionesaldo@gmail.com>
2019-05-13 14:32:54 -0500

    Comenzando pruebas base de datos

--- src/mx/edu/itch/isc/infocoming/interfacesbd/InterfazBD.java
+++ src/mx/edu/itch/isc/infocoming/interfacesbd/InterfazBD.java
@@ -1,5 +1,20 @@
 package mx.edu.itch.isc.infocoming.interfacesbd;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+
 public class InterfazBD {
+    
+    private String url = "jdbc:mysql://localhost:3306/Infocoming?serverTimezone=Mexico/General";
+    private String usuario;
+    private String contrasena;
+    private Connection cc;
+    private Statement st;
+    private ResultSet rs;
+    private ResultSetMetaData rsmd;
+    private Object[] encabezados;
+    private Object[][] datos;
 
 }
---
+++ src/mx/edu/itch/isc/infocoming/manejadores/ManejadorValidarUsuario.java
@@ -0,0 +1,19 @@
+package mx.edu.itch.isc.infocoming.manejadores;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import mx.edu.itch.isc.infocoming.interfacesbd.InterfazBD;
+import mx.edu.itch.isc.infocoming.interfacesgraficas.VValidarUsuario;
+
+public class ManejadorValidarUsuario implements ActionListener{
+    
+    private VValidarUsuario vista;
+    private InterfazBD conexion;
+    
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+
+}
