e33d4954ccc6304236ef6c7e0b1c44df199b54ca
Guirdo <produccionesaldo@gmail.com>
2019-05-14 08:32:12 -0500

    Pruebas con consulta y PanelPrincipalEquipo

--- src/mx/edu/itch/isc/infocoming/manejadores/ManejadorPrincipal.java
+++ src/mx/edu/itch/isc/infocoming/manejadores/ManejadorPrincipal.java
@@ -1,5 +1,59 @@
 package mx.edu.itch.isc.infocoming.manejadores;
 
-public class ManejadorPrincipal {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
+import mx.edu.itch.isc.infocoming.interfacesbd.InterfazBD;
+import mx.edu.itch.isc.infocoming.interfacesbd.InterfazBDEquipo;
+import mx.edu.itch.isc.infocoming.interfacesgraficas.PanelPrincipalEquipo;
+import mx.edu.itch.isc.infocoming.interfacesgraficas.VValidarUsuario;
+
+public class ManejadorPrincipal implements ActionListener {
+
+    private InterfazBD intBD;
+
+    private PanelPrincipalEquipo ppe;
+
+    public ManejadorPrincipal() {
+        new ManejadorValidarUsuario(new VValidarUsuario());
+    }
+
+    public ManejadorPrincipal(InterfazBD inter, PanelPrincipalEquipo p) {
+        this.ppe = p;
+        this.intBD = inter;
+
+        ppe.btnConsultarAlu.addActionListener(this);
+        ppe.btnConsultarPersonal.addActionListener(this);
+
+        ppe.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == ppe.btnConsultarAlu){
+            try {
+                this.manejaEventobtnConsultarAlumno();
+            } catch (SQLException ex) {
+                System.out.println("Error de consulta");
+            }
+        }else if(e.getSource() == ppe.btnConsultarPersonal){
+            this.manejaEventobtnConsultarPersonal();
+        }
+
+    }
+
+    private void manejaEventobtnConsultarAlumno() throws SQLException {
+        Object[][] datos = new InterfazBDEquipo(intBD).consultarAlumnos();
+        
+        ppe.tabla.setModel(new DefaultTableModel(datos, new Object[]{"idAlumno","nombre","apePat","apeMat","Domicilio","Tel"}));
+        
+    }
+
+    private void manejaEventobtnConsultarPersonal() {
+        System.out.println("Estas consultado al personal");
+    }
 
 }
--- src/mx/edu/itch/isc/infocoming/principal/Principal.java
+++ src/mx/edu/itch/isc/infocoming/principal/Principal.java
@@ -1,39 +1,13 @@
 package mx.edu.itch.isc.infocoming.principal;
 
-<<<<<<< HEAD
-import mx.edu.itch.isc.infocoming.interfacesgraficas.DMConfirmarBaja;
-import mx.edu.itch.isc.infocoming.interfacesgraficas.DMCrearGrupo;
-import mx.edu.itch.isc.infocoming.interfacesgraficas.PanelDirector;
-import mx.edu.itch.isc.infocoming.interfacesgraficas.VBajaAlumno;
-import mx.edu.itch.isc.infocoming.interfacesgraficas.VVisualizarAlumnos;
-=======
-import mx.edu.itch.isc.infocoming.interfacesgraficas.*;
-import mx.edu.itch.isc.infocoming.manejadores.ManejadorValidarUsuario;
->>>>>>> 8d6dc27e30e93c3f7b9a44073b69ad6712945535
-
+import mx.edu.itch.isc.infocoming.manejadores.ManejadorPrincipal;
 
 public class Principal {
     
     public static void main(String[] args) {
-<<<<<<< HEAD
-        //new VValidarUsuario().setVisible(true);
-        //new VGestionPagos().setVisible(true);
-        //new DMRegistrarPago().setVisible(true);
-        //new DMRegistroES().setVisible(true);
-        //new VGestionUsuario().setVisible(true);
-        //new VVisualizarAlumnos().setVisible(true);
-        //new DMCrearGrupo().setVisible(true);
-//        new VBajaAlumno().setVisible(true);
-        new PanelDirector().setVisible(true);
         
-//        new DMConfirmarBaja().setVisible(true);
+        new ManejadorPrincipal();
         
-//        System.exit(0);
-=======
-        
-        ManejadorValidarUsuario mvu = new ManejadorValidarUsuario(new VValidarUsuario());
-        
->>>>>>> 8d6dc27e30e93c3f7b9a44073b69ad6712945535
     }
 
 }
